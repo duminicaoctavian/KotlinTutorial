8b7c1b52881af71e78b9033bb15d3ec431c07467
Octavian Duminica <duminicaoctavian@gmail.com>
2020-10-03 15:54:08 +0300

    Conditions

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -26,4 +26,43 @@ fun main() {
     println("My name is $tim")
     println("I can print \$tim")
     println("jim is $jim")
+
+    val lives = 0
+
+    var isGameOver = (lives < 1)
+    println(isGameOver)
+
+    if (isGameOver) {
+        println("Game over!")
+    } else {
+        println("You're still alive!")
+    }
+
+    println("How old are you: ")
+    val age = readLine()!!.toInt()
+    println("Age is $age")
+
+    var message: String
+
+    message = when {
+        age < 18 -> {
+            "You're too young to vote"
+        }
+        age == 100 -> {
+            "Congratulations"
+        }
+        else -> {
+            "You can vote"
+        }
+    }
+
+//    message = if (age < 18) {
+//        "You're too young to vote"
+//    } else if (age == 100) {
+//        "Congratulations"
+//    } else {
+//        "You can vote"
+//    }
+
+    println(message)
 }
