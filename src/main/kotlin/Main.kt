913ab0958274a15f7e57138d7254cf0725e580b6
Octavian Duminica <duminicaoctavian@gmail.com>
2020-10-03 14:36:53 +0300

    More on Variables and Types

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -2,15 +2,22 @@ fun main(args: Array<String>) {
     println("Hello World")
     println("My first Kotlin program")
 
-    var a: String = "Variable a"
-    println(a)
+    var tim: String
+    tim = "Tim Buchalka"
+    println(tim)
 
-    var b: String
-    b = "Variable b"
-    println(b)
+    var timsWeeklySalary: Int = 32
+    var timsMonthlySalary: Int = timsWeeklySalary * 4
+    println(timsWeeklySalary)
+    println(timsMonthlySalary)
 
-    var salary: Int = 32
-    var monthly: Int = salary * 4
-    println(salary)
-    println(monthly)
+    val apples: Int = 6
+    val oranges: Int = 5
+    var fruit: Int = apples + oranges
+    println(fruit)
+    println(apples / 4)
+
+    val weeks: Int = 130
+    val years: Double = weeks / 52.0
+    println(years)
 }
