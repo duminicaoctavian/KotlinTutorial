f9791cc4af1e91e622f329fb065fec8af4533386
Octavian Duminica <duminicaoctavian@gmail.com>
2020-10-03 15:34:42 +0300

    val and var and more on Declarations

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,28 +1,29 @@
-fun main(args: Array<String>) {
+fun main() {
     println("Hello World")
     println("My first Kotlin program")
 
-    var tim: String
-    tim = "Tim Buchalka"
+    val jim = "James T Kirk"
+    val tim = "Tim Buchalka"
     println(tim)
 
-    var timsWeeklySalary: Int = 32
-    var timsMonthlySalary: Int = timsWeeklySalary * 4
+    val timsWeeklySalary = 32
+    val timsMonthlySalary = timsWeeklySalary * 4
     println("Tim's $ weekly salary is $$timsWeeklySalary")
     println("Tim's monthly salary is $timsMonthlySalary")
 
-    val apples: Int = 6
-    val oranges: Int = 5
-    var fruit: Int = apples - oranges
+    val apples = 6
+    val oranges = 5
+    val fruit = apples - oranges
     println("$apples apples - $oranges oranges leaves $fruit piece(s)")
 
     println("A quarter of the apples is ${apples / 4}")
 
-    val weeks: Int = 234
-    val years: Double = weeks / 52.0
+    val weeks = 234
+    val years = weeks / 52.0
     println(years)
     println("$weeks weeks is $years in years")
 
     println("My name is $tim")
     println("I can print \$tim")
+    println("jim is $jim")
 }
