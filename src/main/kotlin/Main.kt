0194382aac09592ff37e5c0d9970157d92ab5c52
Octavian Duminica <duminicaoctavian@gmail.com>
2020-10-04 01:01:50 +0300

    for Loop Revisited

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -177,20 +177,37 @@ fun main() {
     vlad.takeDamage(8)
     println(vlad)
 
-    val dracula = VampyreKing("Dracula")
-    println(dracula)
+    for (i in 1 .. 10) {
 
-    while (dracula.lives > 0) {
+        val dracula = VampyreKing("Dracula")
+        println(dracula)
 
-        if (dracula.dodges()) {
-            continue
+        while (dracula.lives > 0) {
+
+            if (dracula.dodges()) {
+                continue
+            }
+
+            if (dracula.runAway()) {
+                println("Dracula ran away")
+                break
+            } else {
+                dracula.takeDamage(80)
+            }
         }
-
-        if (dracula.runAway()) {
-            println("Dracula ran away")
-            break
-        } else {
-            dracula.takeDamage(12)
-        }
+        println("-----------------------------------------")
     }
+
+    val conan = Player("Conan")
+    conan.getLoot(Loot("Invisibility", LootType.POTION, 4.0))
+    conan.getLoot(Loot("Mithril", LootType.ARMOR, 183.0))
+    conan.getLoot(Loot("Ring of Speed", LootType.RING, 25.0))
+    conan.getLoot(Loot("Red Potion", LootType.POTION, 2.0))
+    conan.getLoot(Loot("Cursed Shield", LootType.ARMOR, -8.0))
+    conan.getLoot(Loot("Brass Ring", LootType.RING, 1.0))
+    conan.getLoot(Loot("Chain Mail", LootType.ARMOR, 4.0))
+    conan.getLoot(Loot("Gold Ring", LootType.RING, 12.0))
+    conan.getLoot(Loot("Health Potion", LootType.POTION, 3.0))
+    conan.getLoot(Loot("Silver Ring", LootType.RING, 6.0))
+    conan.showInventory()
 }
