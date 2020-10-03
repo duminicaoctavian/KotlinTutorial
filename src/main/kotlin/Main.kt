548956f13c6cb97525b22edf8b34e59a11fd9e80
Octavian Duminica <duminicaoctavian@gmail.com>
2020-10-03 14:20:32 +0300

    Variables and Types

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,4 +1,16 @@
 fun main(args: Array<String>) {
     println("Hello World")
     println("My first Kotlin program")
+
+    var a: String = "Variable a"
+    println(a)
+
+    var b: String
+    b = "Variable b"
+    println(b)
+
+    var salary: Int = 32
+    var monthly: Int = salary * 4
+    println(salary)
+    println(monthly)
 }
