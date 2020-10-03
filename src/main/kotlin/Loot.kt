1002a4bb4193ee90f7da6e6d19b222e9925e0523
Octavian Duminica <duminicaoctavian@gmail.com>
2020-10-03 17:16:20 +0300

    toString Challenge

--- src/main/kotlin/Loot.kt
+++ src/main/kotlin/Loot.kt
@@ -4,4 +4,8 @@ enum class LootType {
 }
 
 class Loot(val name: String, val type: LootType, val value: Double) {
+
+    override fun toString(): String {
+        return "$name is $type and is worth $value"
+    }
 }
