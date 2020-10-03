5513619fc72bd0b90bc8eb9081b699afe8873b1a
Octavian Duminica <duminicaoctavian@gmail.com>
2020-10-03 14:47:29 +0300

    String Concatenation and Interpolation

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -8,16 +8,21 @@ fun main(args: Array<String>) {
 
     var timsWeeklySalary: Int = 32
     var timsMonthlySalary: Int = timsWeeklySalary * 4
-    println(timsWeeklySalary)
-    println(timsMonthlySalary)
+    println("Tim's $ weekly salary is $$timsWeeklySalary")
+    println("Tim's monthly salary is $timsMonthlySalary")
 
     val apples: Int = 6
     val oranges: Int = 5
-    var fruit: Int = apples + oranges
-    println(fruit)
-    println(apples / 4)
+    var fruit: Int = apples - oranges
+    println("$apples apples - $oranges oranges leaves $fruit piece(s)")
 
-    val weeks: Int = 130
+    println("A quarter of the apples is ${apples / 4}")
+
+    val weeks: Int = 234
     val years: Double = weeks / 52.0
     println(years)
+    println("$weeks weeks is $years in years")
+
+    println("My name is $tim")
+    println("I can print \$tim")
 }
